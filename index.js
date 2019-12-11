a9245e4f7d30073616b962fcb6e367e6596be8d5
SAGE-DESA <guido.garate@gmail.com>
2019-12-11 10:35:27 -0400

    subiendo plantilla nube2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/docs/prod"));
+app.use(express.static(__dirname + "/Template/layout_1/LTR/material/full"));
 
 app.listen(port, () => {
   console.log(`Escuchando peticiones en el puerto ${port}`);
