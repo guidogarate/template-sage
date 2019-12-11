a8872ec3e8ffea44869baa5ee8ad872c08b396c6
SAGE-DESA <guido.garate@gmail.com>
2019-12-11 13:51:38 -0400

    nube libreria

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/Template/layout_1/LTR/material/full"));
+// app.use(express.static(__dirname + "/Template/layout_1/LTR/material/full"));
 app.use(express.static(__dirname + "/libreria"));
 
 app.listen(port, () => {
