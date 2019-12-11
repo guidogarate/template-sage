23dcce4984f0b7f17e3f5b2116d337c281d743d2
SAGE-DESA <guido.garate@gmail.com>
2019-12-11 13:49:00 -0400

    add libreria

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/Template/layout_1/LTR/material/full"));
+app.use(express.static(__dirname + "/libreria"));
 
 app.listen(port, () => {
   console.log(`Escuchando peticiones en el puerto ${port}`);
